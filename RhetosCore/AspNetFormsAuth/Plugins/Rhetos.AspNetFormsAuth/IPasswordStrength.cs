228fdbf56b2499dfa59800aa36564768049457bd
huy.luong <raymondhuy177@gmail.com>
2017-10-17 17:15:46 +0700

    add Rhetos.AspNetFormsAuth

---
+++ RhetosCore/AspNetFormsAuth/Plugins/Rhetos.AspNetFormsAuth/IPasswordStrength.cs
@@ -0,0 +1,13 @@
+﻿using Rhetos.Dom.DefaultConcepts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rhetos.AspNetFormsAuth
+{
+    public interface IPasswordStrength : IEntity
+    {
+        string RegularExpression { get; set; }
+        string RuleDescription { get; set; }
+    }
+}
