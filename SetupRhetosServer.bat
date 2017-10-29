38b590acc068c9b03e67a3e66f4e078db740e786
RaymondHuy <raymondhuy177@gmail.com>
2017-10-29 19:38:00 +0700

    Update SetupRhetosServer.bat

--- SetupRhetosServer.bat
+++ SetupRhetosServer.bat
@@ -3,4 +3,4 @@ SET databaseName=%2%
 ECHO %sqlServer%
 CD RhetosCore\Source\Rhetos\bin\Debug\netcoreapp2.0\publish
 dotnet CreateAndSetDatabase.dll %sqlServer% %databaseName%
-dotnet DeployPackage.dll
+dotnet DeployPackages.dll
